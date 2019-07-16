78c0fe640417baf2ef6e0d3208296a68bcc05a77
Dave <DaveBathnes@users.noreply.github.com>
2019-07-16 19:15:56 +0100

    #23 - Extending PDF timeout

--- helpers/pdf.js
+++ helpers/pdf.js
@@ -4,7 +4,8 @@ const pug = require('pug');
 const options = {
 	format: 'A4',
 	orientation: 'Portrait',
-	border: '2.54cm'
+	border: '2.54cm',
+	timeout: 100000
 };
 
 module.exports.createPDFStream = (template, values) => new Promise(((resolve, reject) => {
