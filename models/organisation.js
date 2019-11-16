0197ba414dd0b534a57319626383635f48526b2c
Dave <DaveBathnes@users.noreply.github.com>
2019-11-16 09:52:28 +0000

    #31 Adding colour and logo fields to organisations. Closes #31

--- models/organisation.js
+++ models/organisation.js
@@ -1,10 +1,11 @@
 const pool = require('../helpers/database');
+const view_fields = ['id', 'code', 'name', 'timetable', 'website', 'email', 'colour', 'logo', 'number_mobiles', 'number_routes', 'number_stops'];
 
 // Get Organisations: 
 module.exports.getOrganisations = async () => {
 	let organisations = [];
 	try {
-		const { rows } = await pool.query('SELECT id, code, name, timetable, website, email, number_mobiles, number_routes, number_stops FROM vw_organisations');
+		const { rows } = await pool.query('select ' + view_fields.join(', ') + ' from vw_organisations');
 		organisations = rows;
 	} catch (e) { }
 	return organisations;
@@ -14,7 +15,7 @@ module.exports.getOrganisations = async () => {
 module.exports.getOrganisationById = async (id) => {
 	let organisation = null;
 	try {
-		const { rows } = await pool.query('SELECT id, code, name, timetable, website, email, number_mobiles, number_routes, number_stops FROM vw_organisations where id = $1', [id]);
+		const { rows } = await pool.query('select ' + view_fields.join(', ') + ' from vw_organisations where id = $1', [id]);
 		if (rows.length > 0) organisation = rows[0];
 	} catch (e) { }
 	return organisation;
